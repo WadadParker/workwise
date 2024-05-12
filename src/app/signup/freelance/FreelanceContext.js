a5aac9b5d02f45515c1248607e444333500bc08d
WadadParker <wadadparker5@gmail.com>
2024-05-12 08:33:10 +0530

    feat: Context handling logic

--- src/app/signup/freelance/FreelanceContext.js
+++ src/app/signup/freelance/FreelanceContext.js
@@ -1,13 +1,30 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
 
 const FreelanceContext = createContext();
 
 export const FreelanceProvider = ({children}) => {
 
-    
+    const FreelanceReducer = (state,{type,payload,inputField}) => {
+        switch(type) {
+            case "INPUT_FIELDS":
+                return {...state,[inputField]:payload}
+        }
+    }
+
+    const initialState = {
+        name:"",
+        email:"",
+        password:"",
+        hourlyRate:"",
+        domain:"",
+        domain:[],
+        skills:[]
+    }
+
+    const [state,dispatch] = useReducer(FreelanceReducer,initialState);
 
     return (
-        <FreelanceContext.Provider value={{}} >
+        <FreelanceContext.Provider value={{state,dispatch}} >
             {children}
         </FreelanceContext.Provider>
     )
